c779dc4c441a9015fcf7509be57581891eb9d037
AleevRustam <re1973@mail.ru>
2024-02-20 10:24:40 +0200

    2.1. Тест на ValidateInput [#311887 #481753]

--- src/test/java/ru/job4j/tracker/input/ValidateTest.java
+++ src/test/java/ru/job4j/tracker/input/ValidateTest.java
@@ -31,18 +31,14 @@ class ValidateInputTest {
     @Test
     void whenMultiplyValidInput() {
         Output output = new StubOutput();
-        Input in1 = new MockInput(new String[]{"1"});
-        ValidateInput input1 = new ValidateInput(output, in1);
-        int selected1 = input1.askInt("Enter menu:");
-        assertThat(selected1).isEqualTo(1);
-        Input in2 = new MockInput(new String[]{"2"});
-        ValidateInput input2 = new ValidateInput(output, in2);
-        int selected2 = input2.askInt("Enter menu:");
-        assertThat(selected2).isEqualTo(2);
-        Input in3 = new MockInput(new String[]{"3"});
-        ValidateInput input3 = new ValidateInput(output, in3);
-        int selected3 = input3.askInt("Enter menu:");
-        assertThat(selected3).isEqualTo(3);
+        String[] answers = new String[]{"1", "2", "3", "4"};
+        Input in = new MockInput(answers);
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(1);
+        assertThat(selected).isEqualTo(1);
+        assertThat(selected).isEqualTo(1);
+        assertThat(selected).isEqualTo(1);
 
     }
 
