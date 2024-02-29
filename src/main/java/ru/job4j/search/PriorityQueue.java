560651f2dfd81b50659c8041a62f2ccd67027429
AleevRustam <re1973@mail.ru>
2024-02-29 12:07:29 +0200

    5. Очередь с приоритетом на LinkedList [#41670]

---
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -0,0 +1,26 @@
+package ru.job4j.search;
+
+import java.util.LinkedList;
+
+public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+    public void put(Task task) {
+        int index = 0;
+        if (tasks.isEmpty()) {
+            index = 0;
+        } else {
+            for (Task element : tasks) {
+                if (element.getPriority() <= task.getPriority()) {
+                    index++;
+                }
+            }
+        }
+
+        this.tasks.add(index, task);
+    }
+
+    public Task take() {
+        return tasks.poll();
+    }
+}
