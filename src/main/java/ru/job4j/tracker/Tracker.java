d2447627686c91ff18c395e4c40a9870d73cb7ac
AleevRustam <re1973@mail.ru>
2024-03-01 12:46:17 +0200

    9. Изменить программу Tracker из 2-го модуля [#10039 #483930]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,40 +1,38 @@
 package ru.job4j.tracker;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Tracker {
-    private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
+    private List<Item> items = new ArrayList<>();
 
     public Item add(Item item) {
         item.setId(ids++);
-        items[size++] = item;
+        items.add(item);
         return item;
     }
 
-    public Item[] findAll() {
-        return Arrays.copyOf(items, size);
+    public List<Item> findAll() {
+        return List.copyOf(items);
     }
 
-    public Item[] findByName(String key) {
-        Item[] result = new Item[size];
-        int count = 0;
-        for (int i = 0; i < size; i++) {
-            Item item = items[i];
+    public List<Item> findByName(String key) {
+        List<Item> result = new ArrayList<>();
+        for (Item item : items) {
             if (key.equals(item.getName())) {
-                result[count] = item;
-                count++;
+                result.add(item);
             }
         }
-        return Arrays.copyOf(result, count);
+        return result;
     }
 
     private int indexOf(int id) {
         int result = -1;
-        for (int index = 0; index < size; index++) {
-            if (items[index].getId() == id) {
-                result = index;
+        for (Item item : items) {
+            if (item.getId() == id) {
+                result = items.indexOf(item);
                 break;
             }
         }
@@ -43,7 +41,7 @@ public class Tracker {
 
     public Item findById(int id) {
         int index = indexOf(id);
-        return index != -1 ? items[index] : null;
+        return index != -1 ? items.get(index) : null;
     }
 
     public boolean replace(int id, Item updateItem) {
@@ -51,7 +49,7 @@ public class Tracker {
         boolean result = index != -1;
         if (result) {
             updateItem.setId(id);
-            items[index] = updateItem;
+            items.set(index, updateItem);
         }
         return result;
     }
@@ -59,9 +57,7 @@ public class Tracker {
     public void delete(int id) {
         int index = indexOf(id);
         if (index != -1) {
-            System.arraycopy(items, index + 1, items, index, size - index - 1);
-            items[size - 1] = null;
-            size--;
+            items.remove(index);
         }
     }
 }
