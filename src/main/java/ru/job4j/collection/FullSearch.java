2519ac605fec4ded27ff7722d6997c7e93cce4d1
AleevRustam <re1973@mail.ru>
2024-03-04 10:10:45 +0200

    1. Уникальные задачи. [#212653 #484145]

--- src/main/java/ru/job4j/collection/FullSearch.java
+++ src/main/java/ru/job4j/collection/FullSearch.java
@@ -4,10 +4,9 @@ import java.util.HashSet;
 import java.util.List;
 
 public class FullSearch {
-    private HashSet<String> set;
 
     public HashSet<String> extractNumber(List<Task> list) {
-        set = new HashSet<>();
+        HashSet<String> set = new HashSet<>();
         for (Task task : list) {
             set.add(task.getNumber());
         }
