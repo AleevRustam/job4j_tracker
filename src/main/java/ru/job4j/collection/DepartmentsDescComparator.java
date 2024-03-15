05033666e095f3322e244db55852ca789eda170a
AleevRustam <re1973@mail.ru>
2024-03-15 10:51:57 +0200

    3. Отсортировать департаменты [#34352 #486064]

--- src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
+++ src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
@@ -5,32 +5,9 @@ import java.util.Comparator;
 public class DepartmentsDescComparator implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        String leftStartWith = getStartWith(left);
-        String rightStartWith = getStartWith(right);
-        String leftEhdWith = getEndWith(left);
-        String rightEndWith = getEndWith(right);
-        if (leftStartWith.compareTo(rightStartWith) == 0) {
-            for (int i = 0; i < Math.min(leftEhdWith.length(), rightEndWith.length()); i++) {
-                if (leftEhdWith.charAt(i) != rightEndWith.charAt(i)) {
-                    return Character.compare(leftEhdWith.charAt(i), rightEndWith.charAt(i));
-                }
-            }
-            return Integer.compare(leftEhdWith.length(), rightEndWith.length());
+        if (right.split("/")[0].compareTo(left.split("/")[0]) == 0) {
+            return left.compareTo(right);
         }
-        return rightStartWith.compareTo(leftStartWith);
-    }
-
-    private String getStartWith(String string) {
-        if (string.contains("/")) {
-            return string.substring(0, string.indexOf("/"));
-        }
-        return string;
-    }
-
-    private String getEndWith(String string) {
-        if (string.contains("/")) {
-            return string.substring(string.indexOf("/"));
-        }
-        return "";
+        return right.split("/")[0].compareTo(left.split("/")[0]);
     }
 }
