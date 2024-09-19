1c19e860ed4a5287019c6b46c976832edede9d7e
AleevRustam <re1973@mail.ru>
2024-09-19 13:49:20 +0200

    1. Реализация кеша на SoftReference [#1592]

---
+++ src/main/java/ru/job4j/cache/DirFileCache.java
@@ -0,0 +1,24 @@
+package ru.job4j.cache;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class DirFileCache extends AbstractCache<String, String> {
+    private final String cachingDir;
+
+    public DirFileCache(String cachingDir) {
+        this.cachingDir = cachingDir;
+    }
+
+    @Override
+    protected String load(String key) {
+        Path filePath = Paths.get(cachingDir, key);
+        try {
+            return Files.readString(filePath);
+        } catch (IOException e) {
+            return null;
+        }
+    }
+}
