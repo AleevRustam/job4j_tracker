66c3dbb333f054e52ce8010b84f423f377623fba
AleevRustam <re1973@mail.ru>
2024-09-30 09:38:05 +0200

    4. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.template;
+
+import org.junit.jupiter.api.Test;
+import java.util.HashMap;
+import java.util.Map;
+import static org.assertj.core.api.Assertions.*;
+
+class GeneratorTest {
+
+    @Test
+    void whenAllKeysPresentThenCorrectResult() {
+        GeneratorImpl generator = new GeneratorImpl();
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "Petr Arsentev");
+        args.put("subject", "you");
+        String template = "I am a ${name}, Who are ${subject}?";
+        String result = generator.produce(template, args);
+        assertThat(result).isEqualTo("I am a Petr Arsentev, Who are you?");
+    }
+
+    @Test
+    void whenKeyMissingInMapThenThrowException() {
+        GeneratorImpl generator = new GeneratorImpl();
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "Petr Arsentev");
+        String template = "I am a ${name}, Who are ${subject}?";
+        assertThatThrownBy(() -> generator.produce(template, args))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void whenExtraKeyInMapThenThrowException() {
+        GeneratorImpl generator = new GeneratorImpl();
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "Petr Arsentev");
+        args.put("subject", "you");
+        args.put("extra", "extraValue");
+        String template = "I am a ${name}, Who are ${subject}?";
+        assertThatThrownBy(() -> generator.produce(template, args))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
