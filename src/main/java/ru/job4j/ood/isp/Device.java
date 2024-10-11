a5449bb5457c7660823136d2f08d80cfa1f65b92
AleevRustam <re1973@mail.ru>
2024-10-11 11:52:07 +0200

    0. Принцип разделения интерфейсов [#4916]

---
+++ src/main/java/ru/job4j/ood/isp/Device.java
@@ -0,0 +1,24 @@
+package ru.job4j.ood.isp;
+
+interface Device {
+    void playAudio(String audioFile);
+    void displayVideo(String videoFile);
+    void printText(String text);
+}
+
+class AudioPlayer implements Device {
+    @Override
+    public void playAudio(String audioFile) {
+        System.out.println("Playing audio: " + audioFile);
+    }
+
+    @Override
+    public void displayVideo(String videoFile) {
+        throw new UnsupportedOperationException("Video not supported");
+    }
+
+    @Override
+    public void printText(String text) {
+        throw new UnsupportedOperationException("Printing not supported");
+    }
+}
---
+++ src/main/java/ru/job4j/ood/isp/Printer.java
@@ -0,0 +1,24 @@
+package ru.job4j.ood.isp;
+
+interface Printer {
+    void printDocument(String content);
+    void scanDocument(String filePath);
+    void faxDocument(String number);
+}
+
+class SimplePrinter implements Printer {
+    @Override
+    public void printDocument(String content) {
+        System.out.println("Printing: " + content);
+    }
+
+    @Override
+    public void scanDocument(String filePath) {
+        throw new UnsupportedOperationException("Scan not supported");
+    }
+
+    @Override
+    public void faxDocument(String number) {
+        throw new UnsupportedOperationException("Fax not supported");
+    }
+}
