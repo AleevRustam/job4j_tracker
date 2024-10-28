4a7153c74e18b31ee4e78754d1bc23960b754a6a
AleevRustam <re1973@mail.ru>
2024-10-28 14:46:02 +0200

    Очереди и стеки [#505157]

---
+++ src/main/java/ru/job4j/algo/stack/Brackets.java
@@ -0,0 +1,25 @@
+package ru.job4j.algo.stack;
+
+import java.util.Stack;
+
+public class Brackets {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
